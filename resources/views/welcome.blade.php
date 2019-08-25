47325f766af8cef5ef9680118173fc9fbe99e36b
tania <tania.maza@quaira.com>
2019-08-25 18:01:42 -0500

    agregué clases en video

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -59,33 +59,41 @@
                     <img src="{{CRUDBooster::getSetting('slider_3') }}"/>
                 @endif
             </div>
+        
         </div>
 
 <!-- ---- html example --- -->
-        <div>
-        <div class="row multiple-carousel">
-        @foreach($empresas as $empresa)
-        <img src="{{$empresa->logo}}"/>
-        @endforeach
-        </div>
-        </div>
         <div class="container top3">
-            <div class="row justify-content-center align-items-center">
-                    <div class="col-sm-12">
-                        <div align="center" class="embed-responsive embed-responsive-16by9">                   
-                            <video controls class="embed-responsive-item">
-                                <source src="{{$video->url}}" type="video/mp4">
-                            </video>
-                        </div>
-                    </div>
+            <div class="row justify-content-center align-items-center">         
+                <div class="col-sm-12">
+                            <div align="center" class="embed-responsive embed-responsive-16by9" >                   
+                                <video controls class="embed-responsive-item video_actual" data-id="{{$video->id}}">
+                                    <source src="{{$video->url}}" type="video/mp4" >
+                                </video>
+                            </div>
+                </div>
             </div>
         </div>
+        <div class="row" style="margin-top: 100px;">
+            <div class="col-sm-6">
+                <div class="test show-on-scroll">
+                    <p>¿Cómo ser parte de PayMark? Conoce más a cerca de nosotros y empieza a ganar compartiendo.</p>
+                    <button type="button" class="btn btn-warning">Warning</button>
+                </div>
+           
+            </div>   
+            <div class="col-sm-6">
+                <div class="test show-on-scroll">
+                    <p>Regístrate y genera tu link. Empieza a compartir y gana dinero con nosotros.</p>
+                </div>
+            </div>  
+        </div>
         <div class="container-fluid">
-        <div class="row">
+        <div class="row" style="margin-top: 50px; margin-bottom: 50px">
         @foreach($anuncios as $anuncio)
         <div class="col-sm-12 col-lg-3">  
         <figure class="snip1529">
-                <img src="{{$anuncio->imagen}}" alt="pr-sample13" />
+                <img src="{{$anuncio->imagen}}" alt="pr-sample13" style="width:350px;height:350px; object-fit:cover;" />
                 <!-- <figcaption>
                     <h3>An Abstract Post Heading</h3>
                     <p>Which is worse, that everyone has his price, or that the price is always so low.</p>
@@ -96,8 +104,114 @@
         </div>   
         @endforeach
         </div>
+        </div>       
+  
+        <div class="row multiple-carousel">
+        @foreach($empresas as $empresa)
+        <img src="{{$empresa->logo}}"/>
+        @endforeach
+        </div>       
+        
+<!-- Sección A cerca de la empresa -->
+<section id="featured">
+    <div class="container">
+    	<div class="row">
+            <div class="col-sm-12">
+                <div class="titulo show-on-scroll"><span>A cerca de Nosotros</span></div>
+            </div>
         </div>
-        <div class="modal fade pg-show-modal" id="anuncio" tabindex="-1" role="dialog" aria-hidden="true"> 
+        <div class="row" style="margin-top: 100px;">
+            <div class="col-sm-6 contenedor">
+                <img src="uploads/img1.jpg" class="inline-photo">
+                <div class="overlay show-on-scroll"></div>
+            </div>
+            
+            <div class="col-sm-6">
+                <div class="content"><h2>Pixel Facial Cream</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut.</p>
+            </div>
+            </div>
+        </div>
+        <div class="row" style="margin-top: 100px;">
+            <div class="col-sm-6">
+                <div class="content"> <h2>Pixel Facial Cream</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut.</p>
+            </div>
+            </div>   
+            <div class="col-sm-6">
+                <div class="image"><img src="uploads/imagen2.jpg" class="inline-photo show-on-scroll"></div>
+                <div class="overlay show-on-scroll"></div>
+            </div>  
+        </div>
+        
+
+    </div>
+ 
+</section>
+
+<footer class="footer-distributed" style="margin-top: 100px;">
+
+        <div class="footer-left">
+
+            <h3>Company<span>logo</span></h3>
+
+            <p class="footer-links">
+                <a href="#" class="link-1">Home</a>
+                
+                <a href="#">Blog</a>
+            
+                <a href="#">Pricing</a>
+            
+                <a href="#">About</a>
+                
+                <a href="#">Faq</a>
+                
+                <a href="#">Contact</a>
+            </p>
+
+            <p class="footer-company-name">Company Name © 2015</p>
+        </div>
+
+        <div class="footer-center">
+
+            <div>
+                <i class="fa fa-map-marker"></i>
+                <p><span>444 S. Cedros Ave</span> Solana Beach, California</p>
+            </div>
+
+            <div>
+                <i class="fa fa-phone"></i>
+                <p>+1.555.555.5555</p>
+            </div>
+
+            <div>
+                <i class="fa fa-envelope"></i>
+                <p><a href="mailto:support@company.com">support@company.com</a></p>
+            </div>
+
+        </div>
+
+        <div class="footer-right">
+
+            <p class="footer-company-about">
+                <span>About the company</span>
+                Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce euismod convallis velit, eu auctor lacus vehicula sit amet.
+            </p>
+
+            <div class="footer-icons">
+
+                <a href="#"><i class="fa fa-facebook"></i></a>
+                <a href="#"><i class="fa fa-twitter"></i></a>
+                <a href="#"><i class="fa fa-linkedin"></i></a>
+                <a href="#"><i class="fa fa-github"></i></a>
+
+            </div>
+
+        </div>
+
+</footer>
+
+<div class="modal fade pg-show-modal" id="anuncio" tabindex="-1" role="dialog" aria-hidden="true"> 
             <div class="modal-dialog"> 
                 <div class="modal-content"> 
                     <div class="modal-header"> 
@@ -114,8 +228,11 @@
                 </div>                 
             </div>             
         </div>
+
         <script src="{{asset("js/app.js")}}"></script>
         <script type="text/javascript" src="{{asset("js/slick.min.js")}}"></script>
         <script type="text/javascript" src="{{asset("js/index.js")}}"></script>
     </body>
+    
+
 </html>
