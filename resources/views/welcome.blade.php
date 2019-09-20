20a33960eb9817fd012a4b05354882ab59911d5f
Tania <tania.maza@quaira.com>
2019-09-20 15:19:56 -0500

    walla

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -164,11 +164,7 @@
             @foreach($anuncios as $anuncio)
             <div class="col-sm-12 col-lg-3">  
             <figure class="snip1529">
-                    <img src="{{$anuncio->imagen}}" alt="pr-sample13" style="width:350px;height:250px; object-fit:cover;" />
-                    <!-- <figcaption>
-                        <h3>An Abstract Post Heading</h3>
-                        <p>Which is worse, that everyone has his price, or that the price is always so low.</p>
-                    </figcaption>-->
+                    <img src="{{$anuncio->imagen}}" class="img-responsive" />
                     <div class="hover"><i class="ion-android-open"></i></div>
                     <a href="{{$anuncio->url}}" target="_blank"></a>
             </figure>
