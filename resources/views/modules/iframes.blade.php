53e827fb5b71579e87a905a26d47066c54bdecb9
Andres Fabian Diaz <andresdkm@gmail.com>
2019-12-12 17:35:26 -0500

    Cambios dia 1 : (1:15 Hs)

--- resources/views/modules/iframes.blade.php
+++ resources/views/modules/iframes.blade.php
@@ -1,73 +1,130 @@
 @extends('crudbooster::admin_template')
 @section('content')
-    @if($tipo == 'video')
-        <ul class="iframes list-unstyled video-list-thumbs row">
-            @foreach($iframes as $iframe)
-                    <li class="col-lg-4 col-sm-12">
-                        <div class="panel panel-default">
-                            <div class="panel-heading"> {{$iframe->title}}</div>
-                            <div class="panel-body">
-                                <div class="iframe-container">
-                                    {!! $iframe->html !!}
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="box">
+                <div class="box-header">
+                    <h3 class="box-title">
+
+                    </h3>
+                    <div class="box-tools">
+                        <form method="GET" action="" class="form-horizontal">
+                            <input type="hidden" name="tipo" value="{{$tipo}}">
+                            <div class="input-group input-group-sm hidden-xs" style="width: 200px;">
+                                <select class="form-control pull-right" name="enterpriseId" required>
+                                    <option selected value="0">
+                                        Seleccione una empresa
+                                    </option>
+                                    @foreach($enterprises as $enterprise)
+                                        <option value="{{$enterprise->id}}">{{$enterprise->nombre}}</option>
+                                    @endforeach
+                                </select>
+                                <div class="input-group-btn">
+                                    <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
                                 </div>
                             </div>
-                            @if(CRUDBooster::myPrivilegeid()==2)
-                                <div class="panel-footer">
-                                    <a class="btn btn-success btn-edit" title="Editar" href="{{CRUDBooster::mainpath("edit/".$iframe->id."?return_url=".urlencode(Request::fullUrl()))}}"><i class="fa fa-pencil"></i> Editar</a>
-                                    <a class="btn btn-danger btn-delete" title="Eliminar" href="javascript:;" onclick="swal({   
-                                        title: 'Estás Seguro?',   
-                                        text: 'No podrás recuperar estos datos!',   
-                                        type: 'warning',   
-                                        showCancelButton: true,   
-                                        confirmButtonColor: '#ff0000',   
-                                        confirmButtonText: '¡Sí!',  
-                                        cancelButtonText: 'No',  
-                                        closeOnConfirm: false 
-                                        }, 
-                                        function(){  
-                                            location.href= '{{CRUDBooster::mainpath('delete/'.$iframe->id)}}';
-                                        });">
-                                        <i class="fa fa-times"></i> Eliminar
-                                    </a>
-                                </div>
-                            @endif
-                        </div>    
-                    </li>
-            @endforeach
-        </ul>
-    @else
-        <ul class="iframes list-unstyled row">
-            @foreach($iframes as $iframe)
-                    <li class="col-lg-2 col-sm-12 col-xs-6">
-                        <div class="panel panel-default">
-                            <div class="panel-heading"> {{$iframe->title}}</div>
-                            <div class="panel-body">
-                                {!! $iframe->html !!}
-                            </div>
-                            @if(CRUDBooster::myPrivilegeid()==2)
-                            <div class="panel-footer">
-                                <a class="btn btn-success btn-edit" title="Editar" href="{{CRUDBooster::mainpath("edit/".$iframe->id."?return_url=".urlencode(Request::fullUrl()))}}"><i class="fa fa-pencil"></i> Editar</a>
-                                <a class="btn btn-danger btn-delete" title="Eliminar" href="javascript:;" onclick="swal({   
-                                    title: 'Estás Seguro?',   
-                                    text: 'No podrás recuperar estos datos!',   
-                                    type: 'warning',   
-                                    showCancelButton: true,   
-                                    confirmButtonColor: '#ff0000',   
-                                    confirmButtonText: '¡Sí!',  
-                                    cancelButtonText: 'No',  
-                                    closeOnConfirm: false 
-                                    }, 
-                                    function(){  
-                                        location.href= '{{CRUDBooster::mainpath('delete/'.$iframe->id)}}';
-                                    });">
-                                    <i class="fa fa-times"></i> Eliminar
-                                </a>
-                            </div>
-                            @endif
-                        </div>
-                    </li>    
-            @endforeach
-        </ul>
-    @endif
-    <!-- {{ $iframes->appends(['tipo' => $tipo])->links() }} -->
+                        </form>
+                    </div>
+                </div>
+                <!-- /.box-header -->
+                <div class="box-body table-responsive no-padding">
+                    @if($tipo == 'video')
+                        <ul class="iframes list-unstyled video-list-thumbs row">
+                            @foreach($iframes as $iframe)
+                                <li class="col-lg-4 col-sm-12">
+                                    <div class="panel panel-default">
+                                        <div class="panel-heading">
+                                            {{$iframe->title}}
+                                        </div>
+                                        <div class="panel-body">
+                                            <div class="iframe-container">
+                                                {!! $iframe->html !!}
+                                            </div>
+                                        </div>
+                                        @if(CRUDBooster::myPrivilegeid()==2)
+                                            <div class="panel-footer">
+                                                <a class="btn btn-success btn-edit" title="Editar"
+                                                   href="{{CRUDBooster::mainpath("edit/".$iframe->id."?return_url=".urlencode(Request::fullUrl()))}}"><i
+                                                            class="fa fa-pencil"></i> Editar</a>
+                                                <a class="btn btn-danger btn-delete" title="Eliminar"
+                                                   href="javascript:;"
+                                                   onclick="swal({
+                                                           title: 'Estás Seguro?',
+                                                           text: 'No podrás recuperar estos datos!',
+                                                           type: 'warning',
+                                                           showCancelButton: true,
+                                                           confirmButtonColor: '#ff0000',
+                                                           confirmButtonText: '¡Sí!',
+                                                           cancelButtonText: 'No',
+                                                           closeOnConfirm: false
+                                                           },
+                                                           function(){
+                                                           location.href= '{{CRUDBooster::mainpath('delete/'.$iframe->id)}}';
+                                                           });">
+                                                    <i class="fa fa-times"></i> Eliminar
+                                                </a>
+                                            </div>
+                                        @endif
+                                    </div>
+                                </li>
+                            @endforeach
+                        </ul>
+                    @else
+                        <ul class="iframes list-unstyled row">
+                            @foreach($iframes as $iframe)
+                                <li class="col-lg-2 col-sm-12 col-xs-6">
+                                    <div class="panel panel-default">
+                                        <div class="panel-heading"> {{$iframe->title}}</div>
+                                        <div class="panel-body">
+                                            {!! $iframe->html !!}
+                                        </div>
+                                        @if(CRUDBooster::myPrivilegeid()==2)
+                                            <div class="panel-footer">
+                                                <a class="btn btn-success btn-edit" title="Editar"
+                                                   href="{{CRUDBooster::mainpath("edit/".$iframe->id."?return_url=".urlencode(Request::fullUrl()))}}"><i
+                                                            class="fa fa-pencil"></i> Editar</a>
+                                                <a class="btn btn-danger btn-delete" title="Eliminar"
+                                                   href="javascript:;"
+                                                   onclick="swal({
+                                                           title: 'Estás Seguro?',
+                                                           text: 'No podrás recuperar estos datos!',
+                                                           type: 'warning',
+                                                           showCancelButton: true,
+                                                           confirmButtonColor: '#ff0000',
+                                                           confirmButtonText: '¡Sí!',
+                                                           cancelButtonText: 'No',
+                                                           closeOnConfirm: false
+                                                           },
+                                                           function(){
+                                                           location.href= '{{CRUDBooster::mainpath('delete/'.$iframe->id)}}';
+                                                           });">
+                                                    <i class="fa fa-times"></i> Eliminar
+                                                </a>
+                                            </div>
+                                        @endif
+                                    </div>
+                                </li>
+                            @endforeach
+                        </ul>
+                @endif
+                <!-- {{ $iframes->appends(['tipo' => $tipo])->links() }} -->
+                </div>
+                <div class="box-footer">
+                    <ul class="pagination pagination-sm no-margin pull-right">
+                        <li ><a href="{{$iframes->url(1)}}">«</a></li>
+                        @for($i=1; $i<=$iframes->lastPage(); $i++)
+                            <li class="{{ $iframes->currentPage()==$i?'active':''}}"><a href="{{$iframes->url($i)}}">{{$i}}</a></li>
+                        @endfor
+                        @if($iframes->lastPage()>1)
+                            <li><a href="{{$iframes->url($iframes->lastPage())}}">»</a></li>
+                        @endif
+                    </ul>
+                </div>
+                <!-- /.box-body -->
+            </div>
+            <!-- /.box -->
+        </div>
+
+    </div>
+
 @endsection
