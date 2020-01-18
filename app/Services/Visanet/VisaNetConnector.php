47c5a3df16164092dc43913c9e76c092c9f92db8
Renzo Castillo <renzo.castillo@quaira.com>
2020-01-18 09:22:51 -0500

    bug fix en el proceso de activacion del usuario ( no se contemplaba el incremento del patrocinador y del abuelo)

--- app/Services/Visanet/VisaNetConnector.php
+++ app/Services/Visanet/VisaNetConnector.php
@@ -201,21 +201,40 @@ class VisaNetConnector {
 		         ->first();
 	}
 
-	public function activateUser( $userId ) {
-		DB::table( 'cms_users' )
-		  ->where( 'id', $userId )
-		  ->update( [
-			  'activated_at' => Carbon::now(),
-			  'estado'       => 1
-		  ] );
+	public function activateUser( $id ) {
 		$user = DB::table( 'cms_users' )
-		          ->where( 'id', $userId )
-		          ->get()
-		          ->first();
+				->where( 'id', $userId )
+				->get()
+				->first();
+		//evaluamos si el usuario está inactivo para según eso activarlo o no
+		if(!$user->estado){
+			//activamos al usuario en la BD cambiándole su estado
+			DB::table('cms_users')->where('id',$id)->update(['estado'=>1,'activated_at'=>now()]);
+			//aumentamos la cantidad de afiliados actuales del patrocinador en 1
+			if($user->cms_users_id){
+				DB::table('cms_users')->where('id',$user->cms_users_id)->increment('afiliaciones_actuales',1);
+				$abuelo=$this->getAbuelo($user->cms_users_id);
+				if(!empty($abuelo)){
+					//if($abuelo->premium){
+						DB::table('cms_users')->where('id',$abuelo->id)->increment('nietos_actuales',1);
+					//}
+				}
+			}
+		}
+		//mandamos un email a la cuenta de correo de este usuario
 		$link = url( '/' . $user->slug );
 		$data = [ 'nombre' => $user->name, 'link' => $link ];
 		CRUDBooster::sendEmail( [ 'to' => $user->email, 'data' => $data, 'template' => 'activacion_exitosa' ] );
 	}
-
+	
+	public function getAbuelo($idpadre){
+		$padre=DB::table('cms_users')->where('id',$idpadre)->first();
+		if($padre->cms_users_id){
+			$abuelo=DB::table('cms_users')->where('id',$padre->cms_users_id)->first();
+			return $abuelo;
+		}else{
+			return null;
+		}
+	}
 
 }
