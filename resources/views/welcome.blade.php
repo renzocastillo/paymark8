3496ff5a2fb95587d499f7f51dca89716c493cb0
Renzo Castillo <renzo.castillo@quaira.com>
2020-05-25 00:34:00 -0500

    bug fix temporary

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -77,13 +77,13 @@
         </div>
     </div>
     <!-- Sección logos empresas -->
-    <div class="container-fluid">
+    {{--<div class="container-fluid">
             <div class="row multiple-carousel">
             @foreach($empresas as $empresa)
                 <img src="{{url($empresa->logo)}}"/>
             @endforeach
             </div>
-    </div>
+    </div>--}}
     <div class="container-fluid anuncios">
         <h2 class="text-center">{{CRUDBooster::getSetting("cabecera_anuncios")}}</h2>
         <div class="row">
